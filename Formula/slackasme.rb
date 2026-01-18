ce577a8276b53fcf0ace75f0beba11d4d7e3489a
Johnson Chin <jchin@zendesk.com>
2026-01-18 17:05:40 +0800

    Rename formula: slack-cli → slackasme
    - Avoid namespace conflict with Slack's official slack-cli cask
    - Update URLs to new repo name

--- Formula/slackasme.rb
+++ Formula/slackasme.rb
@@ -1,12 +1,12 @@
-class SlackCli < Formula
+class Slackasme < Formula
   desc "Slack CLI - interact with Slack as yourself"
-  homepage "https://github.com/joh90/slack-cli"
+  homepage "https://github.com/joh90/slackasme"
   version "0.1.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/joh90/slack-cli/releases/download/v#{version}/slack-darwin-arm64"
+      url "https://github.com/joh90/slackasme/releases/download/v#{version}/slack-darwin-arm64"
       sha256 "c7ec4a756bc23da772332d4e81af65d497345d1f4802c17fe876e6e342e59e6f"
 
       def install
@@ -15,7 +15,7 @@ class SlackCli < Formula
     end
 
     on_intel do
-      url "https://github.com/joh90/slack-cli/releases/download/v#{version}/slack-darwin-amd64"
+      url "https://github.com/joh90/slackasme/releases/download/v#{version}/slack-darwin-amd64"
       sha256 "97856ecff665bd3711a9535015cc07b115fa62c53dd9a5aaa53c77634f74c337"
 
       def install
@@ -26,7 +26,7 @@ class SlackCli < Formula
 
   on_linux do
     on_intel do
-      url "https://github.com/joh90/slack-cli/releases/download/v#{version}/slack-linux-amd64"
+      url "https://github.com/joh90/slackasme/releases/download/v#{version}/slack-linux-amd64"
       sha256 "3ca71b26fd8e673520894d66f5b8b8cdfac16d9354fa49cbbf639e2809949b0b"
 
       def install
